51ab29a2f8679bf9bedacf93ad9e46fb467da9e5
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-11-21 08:31:10 +0000

    assign the element as center (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
             </header>
             <main className="flex-1">
                 <section className="w-full py-12 md:py-24 lg:py-32">
-                    <div className="container px-4 md:px-6">
+                    <div className="container px-4 md:px-6 flex items-center justify-center">
                         <div className="flex flex-col items-center space-y-4 text-center">
                             <div className="space-y-2">
                                 <h1 className="text-4xl font-headline font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
