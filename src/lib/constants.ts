a5d0752a857cb581f96f3f1688b8d53d335d1df3
Tharvesh588 <tharveshmuhaideen@gmail.com>
2026-02-02 12:02:45 +0530

    change the mech Dept value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const departments = [
   { value: 'CSE', label: 'Computer Science & Engineering' },
   { value: 'ECE', label: 'Electronics & Communication' },
   { value: 'EEE', label: 'Electrical & Electronics' },
-  { value: 'MECH', label: 'Mechanical Engineering' },
+  { value: 'Mech', label: 'Mechanical Engineering' },
   { value: 'Civil', label: 'Civil Engineering' },
   { value: 'IT', label: 'Information Technology' },
   { value: 'AIDS', label: 'Artificial Intelligence & Data Science' },
